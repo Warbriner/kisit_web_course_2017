eadeaac500228996b30bbf58a6c7f5fac3dfb8b6
warbriner <anntonCH@ukr.net>
2018-01-09 16:30:40 +0200

    ...

--- Lab1/Basket.js
+++ Lab1/Basket.js
@@ -1,34 +1,72 @@
-var prodcuts = [{
-	name : "test",
-	price : 12.9,
-	inventory : 20
+var products = [{
+    name: "test",
+    price: 12.9,
+    inventory: 20
 }, {
-	name : "test2",
-	price : 30,
-	inventory : 80
+    name: "test2",
+    price: 30,
+    inventory: 80
 }];
 
 class ProductLineItem {
-	constructor(product) {
-		//implement
-	}
+    constructor(product) {
+        this.name = product.name;
+        this.quantity = 1;
+        this.price = product.price * this.quantity;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getQuantity() {
+        return this.quantity;
+    }
+
+    getPrice() {
+        return this.price;
+    }
+
+    setQuantity(quantity) {
+        this.price /= this.quantity;
+        this.quantity = quantity;
+        this.price *= this.quantity;
+    }
 }
 
+var basket = (function () {
+    var container = [];
 
-var basket = (function(){
-	//implement
-	return {
-		addProduct : function(productID){
-			//implement
-		},
-		removeProduct : function(productID){
-			//implement
-		},
-		updateProductQuantity : function(productID, quantity) {
-			//implement
-		},
-		getTotalPrice : function(){
-			//implement
-		}
-	}
+    return {
+        addProduct: function (PrId) {
+            var exists = false;
+
+            container.forEach(function (item, i, container) {
+                exists = (item.getName() == products[PrId].name)
+            });
+            if (exists) alert("Товар уже существует в корзине");
+            else 
+                var pushed = container.push(new ProductLineItem(products[PrId]))
+        },
+        removeProduct: function (PrId) {
+            container.forEach(function (item, i, container) {
+                if (item.getName() == products[PrId].name) container.splice(i, 1);
+            })
+        },
+        updateProductQuantity: function (PrId, quantity) {
+            container.forEach(function (item, i, container) {
+                if (item.getName() == products[PrId].name) {
+                    item.setQuantity(quantity);
+                }
+            })
+        },
+        getTotalPrice: function () {
+            var totalPrice = 0;
+
+            container.forEach(function (item, i, container) {
+                totalPrice += item.getPrice();
+            })
+            return totalPrice;
+        }
+    }
 })();
